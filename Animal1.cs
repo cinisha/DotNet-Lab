11e2a14203aea3538ad09462ec61f7c5e519b656
cinisha <poudelsinisha@gmail.com>
2026-05-06 16:28:53 +0545

    Add files via upload

---
+++ Animal1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// Base class
+class Animal1
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+}
+
+// Derived class Dog
+class Dog : Animal1
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+// Derived class Cat
+class Cat : Animal1
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Cat meows");
+    }
+}
+
+//// Main class
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Animal1 a;
+
+//        a = new Dog();
+//        a.MakeSound();
+
+//        a = new Cat();
+//        a.MakeSound();
+//    }
+//}
